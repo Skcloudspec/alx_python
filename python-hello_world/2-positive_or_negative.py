2883b7eda0e6f8910b1ed2deec913d53184c9051
Skcloudspec <soukainarizki04@gmail.com>
2023-08-17 16:51:46 -0700

    add 2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 import random
 
-number = random.choice([-98, -2, 0, 3, 8, 98])
+# First random number generator
+number1 = random.choice([-98, -2, 0, 3, 8, 98])
 
-if not isinstance(number, int):
+if not isinstance(number1, int):
     print("TypeError")
-elif number > 0:
-    print("{} is positive".format(number))
-elif number == 0:
+elif number1 > 0:
+    print("{} is positive".format(number1))
+elif number1 == 0:
     print("0 is zero")
 else:
-    print("{} is negative".format(number))
+    print("{} is negative".format(number1))
 
+# Second random number generator
+number2 = random.randint(-10, 10)
+
+if number2 % 2 == 0:
+    print("{} is even".format(number2))
+else:
+    print("{} is odd".format(number2))
