7ef3ac09dfb4784f52e199c86db275b6bb1acdbd
Skcloudspec <soukainarizki04@gmail.com>
2023-08-17 16:30:52 -0700

    add 2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,13 +1,13 @@
 import random
 
-number = random.choice([0, "not a number", -5, 3, 8, -2, 0])
+number = random.choice([0, "not a number", -5, 3, 8, -2, 98, -98])
 
-if type(number) != int:
-    print("{} is not a valid number".format(number))
+if not isinstance(number, int):
+    print("TypeError")
+elif number > 0:
+    print("{} is positive".format(number))
+elif number < 0:
+    print("{} is negative".format(number))
 else:
-    if number > 0:
-        print("{} is a positive number".format(number))
-    elif number < 0:
-        print("{} is a negative number".format(number))
-    else:
-        print("{} is zero".format(number))
+    print("Zero")
+
