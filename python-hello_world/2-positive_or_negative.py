404cf3f72449e3ef8811809c82a9f1e73df37742
Skcloudspec <soukainarizki04@gmail.com>
2023-08-17 16:25:21 -0700

    add 2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
-
 import random
 
-number = random.randint(-10, 10)
+number = random.choice([0, "not a number", -5, 3, 8, -2, 0])
 
-if number % 2 == 0:
-    print("{} is even".format(number))
+if type(number) != int:
+    print("{} is not a valid number".format(number))
 else:
-    print("{} is odd".format(number))
+    if number > 0:
+        print("{} is a positive number".format(number))
+    elif number < 0:
+        print("{} is a negative number".format(number))
+    else:
+        print("{} is zero".format(number))
