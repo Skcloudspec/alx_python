5b65eb075f50ced4b4351d4b16fcb4888faf3a98
Skcloudspec <soukainarizki04@gmail.com>
2023-08-18 08:00:33 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+
 a = 1
 b = 2
 from add_0 import add
-print("{} + {} = {}".format(a, b, add(a, b)))
 
+print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+if _name_ == "_main_":
+    # Main program code goes here
