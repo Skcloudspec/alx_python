02cff6410bb03a9b486b7b4400d193d1f90b72dc
Skcloudspec <soukainarizki04@gmail.com>
2023-08-18 03:46:43 -0700

    1-power.py

---
+++ python-functions/1-power.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def pow(a, b):
+    # initialize result to 1
+    result = 1
+
+    # multiply a to itself b times
+    # if b is negative, divide 1 by a b times
+    for i in range(abs(b)):
+        if b < 0:
+            result /= a
+        else:
+            result *= a
+
