1e227f84ff581430841f87f2a575623f04cafc1e
Skcloudspec <soukainarizki04@gmail.com>
2023-08-18 07:59:40 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 0-import_add.py
-
-from add_0 import add
-
 a = 1
 b = 2
+from add_0 import add
+print("{} + {} = {}".format(a, b, add(a, b)))
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
