3a8d4c525ee6fd8577d3004b52dc4658f8f3878a
Skcloudspec <soukainarizki04@gmail.com>
2023-08-18 06:32:09 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,4 +7,9 @@ b = 2
 print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
 if __name__ == "__main__":
     # Main program code goes here
+a = 1
+b = 2
+from add_0 import add
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
 
