018f9e18df63812a062751f497928fa908f58b6d
Skcloudspec <soukainarizki04@gmail.com>
2023-08-18 04:06:46 -0700

    2-temperature.py

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
+    celsius = round((fahrenheit - 32) * 5/9, 14)  # rounding to 14 decimal places
     return round(celsius, 2)
+
