5adf5cef413fb7fd50eedb42503562ecdb9e2d82
Skcloudspec <soukainarizki04@gmail.com>
2023-08-19 05:15:02 -0700

    0-no_c.py

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    return ''.join([i for i in my_string if i not in 'cC'])
+
+if __name__ == "__main__":
+    print(no_c("Holberton School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
+
