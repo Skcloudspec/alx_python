8116a6d819b425dc5abdaa4f96c1762447cdb05d
Skcloudspec <soukainarizki04@gmail.com>
2023-08-20 11:41:28 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,42 +1,15 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
-"""Defines a square """
-
-
 class Square:
-    """Does nothing"""
-    pass
-Size of the square: 3
-Area of the square with size 3: 9
-Printing the square using '#' character:
+    def __init__(self, size):
+        self.__size = size
 
-###
-###
-###
-
-
-New size of the square: 5
-New area of the square with size 5: 25
-Printing the updated square using '#' character:
-
-#####
-#####
-#####
-#####
-#####
-
-Id of the square: 1
-Size of the square: 0
-Area of the square with default size 0: 0
-size must be an integer
-size must be >= 0
-[Rectangle] (1) 4/6
-[Rectangle] (2) 2/6
-[Rectangle] (2) 2/3
-[Rectangle] (4) 4/3
-[Square] (5) 2/2
-[Square] (5) 3/3
-[Square] (4) 5/5
-size must be an integer
-size must be >= 0
+    def area(self):
+        print(Square.area.__doc__)
+        return self.__size ** 2
+    __doc__ = """
+    this is documentation for my class
+    """
+__doc__ = """
+this is documentation for my module
+"""
 
