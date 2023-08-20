353287dc0f5e87f5fd93741fb9779196e9fee0f2
Skcloudspec <soukainarizki04@gmail.com>
2023-08-20 04:51:18 -0700

    2-variable_load.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -8,5 +8,19 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(result))
 return result
 if __name__ == "__main__":
-    # Code to run when the file is executed as a script
-    safe_print_division(10, 2)
+    # Case when a = 89
+    a = 89
+    b = 2
+    print("When a = {}, b = {}".format(a, b))
+    safe_print_division(a, b)
+
+    # Case when a = -100
+    a = -100
+    b = 20
+    print("\nWhen a = {}, b = {}".format(a, b))
+    safe_print_division(a, b)
+
+    # Case when a is missing
+    b = 0
+    print("\nWhen a is missing, b = {}".format(b))
+    safe_print_division(None, b)
