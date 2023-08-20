11e4076e85afd7eec57316e9cbba14557922fca4
Skcloudspec <soukainarizki04@gmail.com>
2023-08-20 05:33:57 -0700

    1-args.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
+    result = None
+     
     try:
         result = a / b
     except ZeroDivisionError:
-        result = None
+        return None
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
     finally:
         print("Inside result: {}".format(result))
+
     return result
-
-if __name__ == "__main__":
-    # Case when a = 89
-    a = 89
-    b = 2
-    print("When a = {}, b = {}".format(a, b))
-    safe_print_division(a, b)
-
-    # Case when a = -100
-    a = -100
-    b = 20
-    print("\nWhen a = {}, b = {}".format(a, b))
-    safe_print_division(a, b)
-
-    # Case when a is missing
-    b = 0
-    print("\nWhen a is missing, b = {}".format(b))
-    safe_print_division(None, b)
-    from variable_load_2 import a
-
-if __name__ == "__main__":
-    print(a)
