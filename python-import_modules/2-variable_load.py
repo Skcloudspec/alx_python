61358f557b577659c80a312cd7f339a91a00bafc
Skcloudspec <soukainarizki04@gmail.com>
2023-08-20 05:50:56 -0700

    2-variable_load.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    result = None
-     
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        return None
-    except Exception as e:
-        print("An error occurred:", e)
-        return None
-    finally:
-        print("Inside result: {}".format(result))
-
-    return result
+if __name__ == "__main__":
+    import variable_load_2
+    print(variable_load_2.a)
