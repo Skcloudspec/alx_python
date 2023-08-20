6cb38ca2879bf058b2af610ec989b812e1f3312d
Skcloudspec <soukainarizki04@gmail.com>
2023-08-20 11:31:11 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,3 +6,37 @@
 class Square:
     """Does nothing"""
     pass
+Size of the square: 3
+Area of the square with size 3: 9
+Printing the square using '#' character:
+
+###
+###
+###
+
+
+New size of the square: 5
+New area of the square with size 5: 25
+Printing the updated square using '#' character:
+
+#####
+#####
+#####
+#####
+#####
+
+Id of the square: 1
+Size of the square: 0
+Area of the square with default size 0: 0
+size must be an integer
+size must be >= 0
+[Rectangle] (1) 4/6
+[Rectangle] (2) 2/6
+[Rectangle] (2) 2/3
+[Rectangle] (4) 4/3
+[Square] (5) 2/2
+[Square] (5) 3/3
+[Square] (4) 5/5
+size must be an integer
+size must be >= 0
+
