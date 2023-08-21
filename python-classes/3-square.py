421eef1a3d0d758bf93a204a6e780ff480c8f4fd
Skcloudspec <soukainarizki04@gmail.com>
2023-08-21 13:54:00 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,46 +1,32 @@
 #!/usr/bin/python3
-```
-"""
-This is the documentation for the Square class that defines a square by its size
-"""
-
 class Square:
     """
-    This class defines a square by its size
+    This is a class that represents a square.
     """
+
     def __init__(self, size=0):
         """
-        Initializes a Square instance with a given size
-
-        Args:
-            - size (int): the length of a side of the square (default: 0)
+        This method initializes a Square object with a size attribute.
+        :param size: An integer representing the size of the square.
         """
         self.__size = size
 
     @property
     def size(self):
         """
-        Getter method for private instance attribute size
-
-        Returns:
-            - The size of the square
+        This method retrieves the size attribute for the square.
         """
         return self.__size
 
     @size.setter
     def size(self, value):
         """
-        Setter method for private instance attribute size
-
-        Args:
-            - value (int): the length of a side of the square
-
-        Raises:
-            - TypeError: if value is not an integer
-            - ValueError: if value is less than 0
+        This method sets the size attribute for the square.
+        :param value: An integer representing the size of the square.
+        :raises ValueError: If value is not an integer or is less than 0.
         """
         if type(value) != int:
-            raise TypeError("size must be an integer")
+            raise ValueError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
@@ -48,40 +34,39 @@ class Square:
 
     def area(self):
         """
-        Computes and returns the area of the square
-
-        Returns:
-            - The area of the square
+        This method calculates and returns the area of the square.
         """
         return self.__size ** 2
 
 
-# test cases
-if __name__ == '__main__':
+# Test Case 1
+mysquare = Square(89)
+print(mysquare.size)  # output: 89
+print(mysquare.area())  # output: 7921
+mysquare.size = 33
+print(mysquare.size)  # output: 33
+print(mysquare.area())  # output: 1089
+
+# Test Case 2
+mysquare = Square(89)
+print(mysquare.size)  # output: 89
+
+# Test Case 3
+mysquare = Square()
+print(mysquare.size)  # output: 0
+
+# Test Case 4
+mysquare = Square(89)
+print(mysquare.size)  # output: 89
+mysquare.size = 33
+print(mysquare.size)  # output: 33
+
+# Test Case 5
+try:
     mysquare = Square(89)
     print(mysquare.size)  # output: 89
-    print(mysquare.area())  # output: 7921
-    mysquare.size = 33
-    print(mysquare.size)  # output: 33
-    print(mysquare.area())  # output: 1089
+    mysquare.size = "89"
+    print(mysquare.size)
+except Exception as e:
+    print(e)  # output: size must be an integer
 
-    mysquare = Square(89)
-    print(mysquare.size)  # output: 89
-
-    mysquare = Square()
-    print(mysquare.size)  # output: 0
-
-    mysquare = Square(89)
-    print(mysquare.size)  # output: 89
-    mysquare.size = 33
-    print(mysquare.size)  # output: 33
-
-    try:
-        mysquare = Square(89)
-        print(mysquare.size)  # output: 89
-        mysquare.size = "89"
-        print(mysquare.size)
-    except Exception as e:
-        print(e)
-        # output: size must be an integer
-```
