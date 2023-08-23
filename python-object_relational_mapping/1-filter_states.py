9779ccdf39acf45f46e63170eb92c9794ace9794
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 03:57:56 -0700

    1-select_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, database):
+    # Connect to MySQL server
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the query to select states with names starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    # Get command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the function to filter and list states
+    filter_states(username, password, database)
