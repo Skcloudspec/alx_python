598e2d9df7df4a0ad9fda64a687357901a538e7c
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 04:17:20 -0700

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, database, state_name):
+    # Connect to MySQL server
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Prepare the SQL query with user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    # Get command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Call the function to filter and list states
+    filter_states(username, password, database, state_name)
