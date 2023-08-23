aba050e8ccfdb4d7608c1a5ad28914cb2e4c81f9
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 04:43:27 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,44 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+        """
         self.__size = 0
         self.size = size
 
     @property
     def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -17,11 +46,39 @@ class Square:
         self.__size = value
 
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Prints the square to stdout using the '#' character.
+        If the size is 0, it prints an empty line.
+        """
         if self.__size == 0:
             print()
         else:
             for _ in range(self.__size):
                 print("#" * self.__size)
+
+
+# Test cases
+if __name__ == "__main__":
+    mysquare = Square(3)
+    mysquare.my_print()
+
+    print("--")
+
+    mysquare = Square(10)
+    mysquare.my_print()
+
+    print("--")
+
+    mysquare = Square(0)
+    mysquare.my_print()
+
+    print("--")
