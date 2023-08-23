d3534dfea9ec671cf88a13556407428ecab0cba1
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 11:31:03 -0700

    1-_filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,39 @@
 ```python
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N(upper N or lower n)
+Script that lists all states with a name starting with n(lowercase n)
+from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} USERNAME PASSWORD DATABASE".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database = sys.argv[1:]
-
+if __name__ == "__main__":
+    # Connect to the database using the arguments
+    username, password, db_name = sys.argv[1:]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database)
+        db=db_name,
+        charset="utf8")
 
+    # Create a cursor object to execute queries
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC;")
-    rows = cursor.fetchall()
-    for row in rows:
+
+    # Execute the query
+    cursor.execute("""SELECT * FROM states
+                   WHERE name LIKE BINARY 'n%'
+                   ORDER BY id ASC""")
+
+    # Display the results
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
 
+    # Close cursor and database connection
     cursor.close()
     db.close()
