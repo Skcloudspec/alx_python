56d1ba3a153c0d0e06cb27aee64bf01969ee8958
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 04:04:26 -0700

    1-select_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 import MySQLdb
 import sys
 
-
 def list_states(username, password, database):
     # Connect to MySQL server
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
@@ -10,7 +9,7 @@ def list_states(username, password, database):
     # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Execute the query to create the database if it doesn't exist
+    # Create the test_1 database if it doesn't exist
     cursor.execute("CREATE DATABASE IF NOT EXISTS test_1")
 
     # Switch to the test_1 database
@@ -25,8 +24,8 @@ def list_states(username, password, database):
     # Insert sample data into the states table
     cursor.execute("INSERT INTO states (name) VALUES ('California'), ('nevada'), ('New York')")
 
-    # Execute the query to select states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Execute the query to select states with names starting with 'N' (case-insensitive)
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
     # Fetch all rows from the result set
     rows = cursor.fetchall()
@@ -38,7 +37,6 @@ def list_states(username, password, database):
     # Close the cursor and database connection
     cursor.close()
     db.close()
-
 
 if __name__ == '__main__':
     # Get command-line arguments
