109f30d9213e138c222bedad329d80b4aad47c13
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 09:15:35 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,27 +1,20 @@
-from urllib.parse import urlencode
 import requests
 import sys
 
-# Construct the URL with parameters using urllib.parse.urlencode
-base_url = "http://0.0.0.0:5050"
-params = {"email": "test@test.com"}
-
-url = f"{base_url}?{urlencode(params)}"
-print(url)
-
-# Send the POST request
-response = requests.post(url)
-
-# Retrieve the URL and email address from the command-line arguments
 url = sys.argv[1]
 email = sys.argv[2]
 
-# Construct the payload
-payload = {'email': email}
+# Ensure proper formatting of the URL and email
+if not url.startswith("http://") and not url.startswith("https://"):
+    url = "http://" + url
+if not url.endswith("/"):
+    url += "/"
+if not email.startswith("email="):
+    email = "email=" + email
 
-# Send the POST request
-response = requests.post(url, data=payload)
+url += "?" + email
 
-# Print the email address and the body of the response
-print("Your email is:", email)
+response = requests.post(url)
+
+print("Your email is:", email.split("=")[1])
 print(response.text)
