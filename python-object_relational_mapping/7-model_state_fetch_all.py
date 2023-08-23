20412a6ebc1b4d931942b7b305419fe98cd5d8b7
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 12:51:40 -0700

    7-model_state_fetch_all.py

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that lists all State objects from the database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Retrieve MySQL connection credentials
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create engine and session
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Fetch all State objects and sort by id
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
