9b692f43849e30c2bc520a4bf3108ce4378f453f
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 04:36:13 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,43 +1,16 @@
 #!/usr/bin/python3
+
+```
 class Square:
-    """
-    Square class represents a square shape.
-
-    Attributes:
-        size (int): The size of the square.
-    """
-
     def __init__(self, size=0):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The size of the square (default is 0).
-        """
         self.size = size
 
     @property
     def size(self):
-        """
-        Getter method to retrieve the size of the square.
-
-        Returns:
-            int: The size of the square.
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter method to set the size of the square.
-
-        Args:
-            value (int): The size of the square.
-
-        Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -45,56 +18,11 @@ class Square:
         self.__size = value
 
     def area(self):
-        """
-        Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
         return self.__size ** 2
 
     def my_print(self):
-        """
-        Prints the square using the character '#' in the standard output.
-        If the size is 0, it prints an empty line.
-        """
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
+            for i in range(self.__size):
                 print("#" * self.__size)
-
-
-# Example usage
-
-# Create a Square object with size 3
-my_square = Square(3)
-my_square.my_print()
-
-# Correct output:
-# ###
-# ###
-# ###
-
-# Create a Square object with size 10
-my_square = Square(10)
-my_square.my_print()
-
-# Correct output:
-# ##########
-# ##########
-# ##########
-# ##########
-# ##########
-# ##########
-# ##########
-# ##########
-# ##########
-# ##########
-
-# Create a Square object with size 0
-my_square = Square(0)
-my_square.my_print()
-
-# Correct output:
-#
