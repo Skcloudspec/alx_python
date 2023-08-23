5ab42f0e4d16496d01bd33229c5463af9a9c7520
Skcloudspec <soukainarizki04@gmail.com>
2023-08-23 03:36:41 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -36,3 +36,56 @@ print(("--"), end=" ")
 my_square.size = 0
 my_square.my_print()
 print(("--"), end=" ")
+```
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+        print()  # add extra line for spacing
+
+my_square = Square(3)
+my_square.my_print()  # Output:
+"""
+###
+###
+###
+"""
+
+my_square.size = 10
+my_square.my_print()  # Output:
+"""
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+##########
+"""
+
+my_square.size = 0
+my_square.my_print()
