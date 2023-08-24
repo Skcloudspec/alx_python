b8c08dbf461ac5856913c46d93f378b6fcd7a941
Skcloudspec <soukainarizki04@gmail.com>
2023-08-24 03:42:54 -0700

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,35 +1,22 @@
-```python
 #!/usr/bin/python3
-"""Script that displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument"""
+
+"""Module that lists all states from the hbtn_0e_0_usa database."""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # retrieve command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
 
-    # connect to MySQL server
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=mysql_username,
-                         passwd=mysql_password,
-                         db=database_name)
+    # Get MySQL credentials and search name from command-line arguments
+    # and # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    # execute SQL query to retrieve states with matching name
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name='{}' ORDER BY id"
-                   .format(state_name))
+    # Execute the SQL query to retrieve states with the specified name
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
 
-    # fetch all matching rows and display results
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    # close cursor and database connections
-    cursor.close()
-    db.close() 
+    # Fetch all rows and print the states
+    [print(state) for state in c.fetchall()]
+ 
