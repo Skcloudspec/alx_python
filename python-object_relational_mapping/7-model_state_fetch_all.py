b132fad6ed513a3506f05ad5d509e41a3df51e49
Skcloudspec <soukainarizki04@gmail.com>
2023-08-24 04:52:52 -0700

    7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""Script that lists all State objects from the database"""
+"""Module that retrieves and prints all\
+        states from a MySQL database using SQLAlchemy."""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    # Retrieve MySQL connection credentials
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Create engine and session
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Create a session factory
     Session = sessionmaker(bind=engine)
+
+    # Create a session object
     session = Session()
 
-    # Fetch all State objects and sort by id
-    states = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
-    session.close()
+    # Retrieve all states from the database and print their IDs and names
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
