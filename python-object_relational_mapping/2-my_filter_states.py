3b70d5c446193ee992f758842026c5f7cce744da
Skcloudspec <soukainarizki04@gmail.com>
2023-08-24 04:11:16 -0700

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+`#!/usr/bin/python3
 
 """Module that lists all states from the hbtn_0e_0_usa database."""
 
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
+if name == "main":
     # Get MySQL credentials and search name from command-line arguments
     # and Connect to MySQL server
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
@@ -13,9 +13,10 @@ if __name__ == "__main__":
 
     # Execute the SQL query to retrieve states with the specified name
     c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+                 FROM states \
+                WHERE BINARY name = '{}'".format(sys.argv[4]))
 
     # Fetch all rows and print the states
     [print(state) for state in c.fetchall()]
+
 
