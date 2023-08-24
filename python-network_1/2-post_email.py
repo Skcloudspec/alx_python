984b6c1f7d559bbe3a861985cd55cbc44a4fdfc2
Skcloudspec <soukainarizki04@gmail.com>
2023-08-24 06:58:57 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
-"""
+          import requests
 import sys
-import urllib.parse
-import urllib.request
 
+url = sys.argv[1]
+email = sys.argv[2]
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+# Ensure proper formatting of the URL and email
+if not url.startswith("http://") and not url.startswith("https://"):
+    url = "http://" + url
+if not url.endswith("/"):
+    url += "/"
+if not email.startswith("email="):
+    email = "email=" + email
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+# Construct the final URL
+final_url = url + email
+
+response = requests.post(final_url)
+
+print("Your email is:", email.split("=")[1])
+print(final_url)
