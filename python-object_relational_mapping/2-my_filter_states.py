60ca012b5289cbfe67016a4d7902a78385aad3a1
Skcloudspec <soukainarizki04@gmail.com>
2023-08-24 04:07:50 -0700

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,9 +6,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-
     # Get MySQL credentials and search name from command-line arguments
-    # and # Connect to MySQL server
+    # and Connect to MySQL server
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
 
@@ -19,6 +18,4 @@ if __name__ == "__main__":
 
     # Fetch all rows and print the states
     [print(state) for state in c.fetchall()]
-
-
 
