0bea9633f0bbc76e01d0b8a9d74bdc80693ff6ab
Skcloudspec <soukainarizki04@gmail.com>
2023-08-27 11:54:33 -0700

    0-hello_route.py

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,14 +1,10 @@
-from flask import Flask, abort
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
-
-@app.errorhandler(404)
-def not_found(error):
-    return 'Not Found', 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
