d789731d8b97b204e3173c7b13362055e96d4c3d
Skcloudspec <soukainarizki04@gmail.com>
2023-08-27 12:07:10 -0700

    0-hello_route.py

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -3,17 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Route handler for the root URL (/).
-
-    Returns:
-        str: The greeting message 'Hello HBNB!'.
-    """
+def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    """
-    Start the Flask server.
-    """
-
     app.run(host='0.0.0.0', port=5000)
