8134670df7295e09e226a67644753b05e61fa640
Skcloudspec <soukainarizki04@gmail.com>
2023-09-22 06:58:39 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
     response = requests.post(url, data=email)
-    print(response.text)
+    print("Email: {}".format(sys.argv[2]))
     print("[{}]".format(response.status_code))
+    print(response.text.strip())
 ```
-
