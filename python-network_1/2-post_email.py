71a51bfb9ee84df5cfe2bd8ca852b882a7febf93
Skcloudspec <soukainarizki04@gmail.com>
2023-09-22 06:50:43 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,21 +1,10 @@
-          import requests
+```python
+#!/usr/bin/env python3
+import requests
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-# Ensure proper formatting of the URL and email
-if not url.startswith("http://") and not url.startswith("https://"):
-    url = "http://" + url
-if not url.endswith("/"):
-    url += "/"
-if not email.startswith("email="):
-    email = "email=" + email
-
-# Construct the final URL
-final_url = url + email
-
-response = requests.post(final_url)
-
-print("Your email is:", email.split("=")[1])
-print(final_url)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
