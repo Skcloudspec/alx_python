f255fdeac75ec2f0d4a354bcef56a1b40b887245
Skcloudspec <soukainarizki04@gmail.com>
2023-09-22 08:52:50 -0700

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,3 +1,4 @@
+```python
 #!/usr/bin/env python3
 import requests
 import sys
@@ -6,8 +7,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
     response = requests.post(url, data=email)
-    print("Correct output - case: request {} with email={}".format(url, sys.argv[2]))
+    output = '{0} with email={1}'.format(url, sys.argv[2])
+    print("Correct output - case: request {}".format(output))
     print("[{}]".format(response.status_code))
-    print(response.request.url)
-```
-`
+    print(response.request.url.replace(' ', ''))
+````
